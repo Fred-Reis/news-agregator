e82c0ca6890f8a0a60e203d04ffec8d7af202f14
Fred-Reis <fredgost@hotmail.com>
2024-08-06 14:06:43 -0300

    feat: add filters to articles

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,22 +1,58 @@
 "use client";
 
-import { Article } from "@/@types/articles";
+import { useEffect, useMemo } from "react";
+
 import { Card } from "@/components";
 import useMultipleApis from "@/infra/http/hook/useMultipleApis";
 
 import { useArticlesStore } from "@/store/articlesStore";
-import { useEffect } from "react";
-
-interface ApiProps {
-  totalResults: number;
-  articles: Article[];
-  status: string;
-}
+import { filterArticles } from "@/hooks/useFilterArticles";
+import useFilterStore from "@/store/filterStore";
+import usePreferencesStore from "@/store/preferencesStore";
 
 export default function Home() {
-  const { articles, error, loading } = useArticlesStore();
-
+  const { articles, loading } = useArticlesStore();
   const { fetchAllData } = useMultipleApis();
+
+  const { dates, categories, sources } = useFilterStore();
+
+  const {
+    authors,
+    sources: preferedSources,
+    categories: preferedCategories,
+  } = usePreferencesStore();
+
+  const preferedArticles = useMemo(
+    () =>
+      filterArticles({
+        articles,
+        authors: authors
+          .filter((item) => item.checked)
+          .map((item) => item.value),
+        sources: preferedSources
+          .filter((item) => item.checked)
+          .map((item) => item.value),
+        categories: preferedCategories
+          .filter((item) => item.checked)
+          .map((item) => item.value),
+      }),
+    [articles, authors, preferedCategories, preferedSources]
+  );
+
+  const filteredArticles = useMemo(
+    () =>
+      filterArticles({
+        articles: preferedArticles,
+        dates: dates.filter((item) => item.checked).map((item) => item.value),
+        categories: categories
+          .filter((item) => item.checked)
+          .map((item) => item.value),
+        sources: sources
+          .filter((item) => item.checked)
+          .map((item) => item.value),
+      }),
+    [preferedArticles, dates, categories, sources]
+  );
 
   useEffect(() => {
     fetchAllData();
@@ -26,17 +62,13 @@ export default function Home() {
     return <div>Loading...</div>;
   }
 
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
-
   if (articles.length === 0) {
     return <div>No articles found.</div>;
   }
 
   return (
     <section className="grid gap-y-10 p-8 lg:pt-16 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 max-w-[1400px] gap-6 mx-auto  place-content-between">
-      {articles.map((news) => (
+      {filteredArticles.map((news) => (
         <div className="mx-auto" key={news.id}>
           <Card news={news} />
         </div>
