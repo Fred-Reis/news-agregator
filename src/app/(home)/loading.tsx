22cb8b537d8f1528355c5ee86ecefcbcdef767a4
Fred-Reis <fredgost@hotmail.com>
2024-08-07 12:55:07 -0300

    fix: adjust skeleton on small screens

--- src/app/(home)/loading.tsx
+++ src/app/(home)/loading.tsx
@@ -3,14 +3,14 @@ import { Skeleton } from "@/components";
 export default function HomeLoading() {
   return (
     <section className="grid gap-y-10 p-8 lg:pt-16 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 max-w-[1400px] gap-6 mx-auto  place-content-between">
-      <Skeleton className="w-[300px] h-[600px] shadow-2xl" />
-      <Skeleton className="w-[300px] h-[600px] shadow-2xl" />
-      <Skeleton className="w-[300px] h-[600px] shadow-2xl" />
-      <Skeleton className="w-[300px] h-[600px] shadow-2xl" />
-      <Skeleton className="w-[300px] h-[600px] shadow-2xl" />
-      <Skeleton className="w-[300px] h-[600px] shadow-2xl" />
-      <Skeleton className="w-[300px] h-[600px] shadow-2xl" />
-      <Skeleton className="w-[300px] h-[600px] shadow-2xl" />
+      <Skeleton className="w-[440px] sm:w-[300px] h-[208px] sm:h-[600px] shadow-2xl" />
+      <Skeleton className="w-[440px] sm:w-[300px] h-[208px] sm:h-[600px] shadow-2xl" />
+      <Skeleton className="w-[440px] sm:w-[300px] h-[208px] sm:h-[600px] shadow-2xl" />
+      <Skeleton className="w-[440px] sm:w-[300px] h-[208px] sm:h-[600px] shadow-2xl" />
+      <Skeleton className="w-[440px] sm:w-[300px] h-[208px] sm:h-[600px] shadow-2xl" />
+      <Skeleton className="w-[440px] sm:w-[300px] h-[208px] sm:h-[600px] shadow-2xl" />
+      <Skeleton className="w-[440px] sm:w-[300px] h-[208px] sm:h-[600px] shadow-2xl" />
+      <Skeleton className="w-[440px] sm:w-[300px] h-[208px] lg:h-[600px] shadow-2xl" />
     </section>
   );
 }
